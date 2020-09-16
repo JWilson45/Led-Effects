b7c70265a823cb28881936d18356c44924d4743d
Jason Wilson <jason.wilson2@marist.edu>
2020-09-16 14:59:26 -0400

    Performance Upgrades

--- Running Lights Variations/RedPurpleDarkBlue-Running/RedPurpleDarkBlue-Running.ino
+++ Running Lights Variations/RedPurpleDarkBlue-Running/RedPurpleDarkBlue-Running.ino
@@ -10,38 +10,22 @@ void setup()
 
 // *** REPLACE FROM HERE ***
 void loop() {
-  RunningLights(0xff,0,0);        // red
-  RunningLights(0xff,0xff,0xff);  // white
-  RunningLights(0,0,0xff);        // blue
+  RunningLights();
 }
 
-void RunningLights(byte red, byte green, byte blue) {
+void RunningLights() {
+  byte red;
+  byte green;
+  byte blue;
   int Position=0;
   int colorChange = 0;
   
-  for(int j=0; j<NUM_LEDS; j++)
+  for(int j=0; j<NUM_LEDS * 10; j++)
   {
     colorChange++;
     if(colorChange >= 3) {
       colorChange = 0;
-    }
-    
-    for(int k = 0; k <= 1; k++) {
-
-      if(colorChange == 0){
-        red = 0xff;
-        green = 0;
-        blue = 0;
-      } else if( colorChange == 1) {
-        red = 0xff;
-        green = 0xff;
-        blue = 0xff;
-      } else {
-        red = 0;
-        green = 0;
-        blue = 0xff;
-      }
-        
+    }    
       
       Position++; // = 0; //Position + Rate;
       for(int i=0; i<NUM_LEDS; i++) {
@@ -52,46 +36,43 @@ void RunningLights(byte red, byte green, byte blue) {
           colorChange = 0;
         }
 
-    
+
+        // Set colors here:
+
+        // Red
         if(colorChange == 5){
           red = 0xff;
           green = 0;
           blue = 0;
+
+        // Purple
         } else if(colorChange == 10) {
-          red = 0xff;
-          green = 0xff;
-          blue = 0xff;
+          red = 0x80;
+          green = 0;
+          blue = 0x80;
+
+          // Blue
         } else if(colorChange == 15) {
           red = 0;
           green = 0;
           blue = 0xff;
         }
-      
-        // sine wave, 3 offset waves make a rainbow!
-//        float level = sin(i+Position) * 127 + 128;
-//        setPixel(i,level,0,0);
-//        float level = sin(i+Position) * 127 + 128;
+
+        // Sin Wave
         setPixel(i,((sin(i+Position) * 127 + 128)/255)*red,
                    ((sin(i+Position) * 127 + 128)/255)*green,
                    ((sin(i+Position) * 127 + 128)/255)*blue);
+
       }
 
-    }
-      
       showStrip();
   }
 }
 // *** REPLACE TO HERE ***
 
 void showStrip() {
- #ifdef ADAFRUIT_NEOPIXEL_H 
-   // NeoPixel
-   strip.show();
- #endif
- #ifndef ADAFRUIT_NEOPIXEL_H
    // FastLED
    FastLED.show();
- #endif
 }
 
 void setPixel(int Pixel, byte red, byte green, byte blue) {
